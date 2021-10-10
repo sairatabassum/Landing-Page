3ad53aeb573732d2f91c9388274f0154870d38b3
Saira Tabassum <sairatabassum386@gmail.com>
2021-10-10 13:58:14 +0600

    commit

--- Resources/JS/script.js
+++ Resources/JS/script.js
@@ -7,3 +7,30 @@ $(document).ready(function () {
     }
   });
 });
+
+$(".js-about-section").waypoint(
+  function (direction) {
+    $(".js-about-box").addClass("animate__animated animate__fadeIn");
+  },
+  {
+    offset: "50%",
+  }
+);
+
+$(".js-service-box").waypoint(
+  function (direction) {
+    $(".js-service-box").addClass("animate__animated animate__zoomIn");
+  },
+  {
+    offset: "50%",
+  }
+);
+
+$(".js-highlighted").waypoint(
+  function (direction) {
+    $(".js-highlighted").addClass("animate__animated animate__pulse");
+  },
+  {
+    offset: "50%",
+  }
+);
